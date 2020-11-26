328c580fdfe8c047b80f31b2f2f8d83cf7424f1f
oleg <vinodevmail@gmail.com>
2020-11-26 19:42:52 +0200

    improved dark mode toggle btn

--- public/js/darkTheme.js
+++ public/js/darkTheme.js
@@ -2,5 +2,9 @@ const btnToggleDark = document.querySelector(".btn-toggle-dark");
 
 btnToggleDark.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme");
+    btnToggleDark.classList.toggle("btn-toggle-dark-on")
     localStorage.setItem("theme", document.body.className);
 })
+
+if(currentTheme === "dark-theme")
+    btnToggleDark.classList.add("btn-toggle-dark-on")
