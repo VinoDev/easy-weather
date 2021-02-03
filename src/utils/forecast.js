9a96a43088e690ac9aeac6c5ccf11a4c0c6cd44b
oleg <vinodevmail@gmail.com>
2021-02-03 15:44:27 +0200

    changed config.js to dev.env

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,10 +1,7 @@
 const request = require('postman-request');
-const localKey = require('../../config').weatherStack;
-
-const key = process.env.WEATHERSTACK_KEY || localKey;
 
 const forecast = (longtitude, latitude, location, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=${key}&query=${longtitude},${latitude}`;
+    const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_KEY}&query=${longtitude},${latitude}`;
 
     request({url, json: true}, (error, {body})=>{
         if(error) {
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,11 +1,7 @@
 const request = require('postman-request');
-const localKey = require('../../config').mapbox;
-
-const key = process.env.MAPBOX_KEY || localKey;
 
 const geocode = (address, callback) => {
-    const url= `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${key}&limit=1`;
-
+    const url= `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.MAPBOX_KEY}&limit=1`;
     request({url, json: true}, (error, {body} = {})=>{
         if(error) {
             callback("Could not connect to mapbox", undefined);
